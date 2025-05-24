d57fdd8eede16f3f758e364320fe926d61d078ee
Vincent Joy <vincentjoyhere@gmail.com>
2025-05-24 10:27:36 +0530

    landing page for the app made with dummy data

---
+++ FitnessAgent/NavigationStackApp/NavigationStackApp.swift
@@ -0,0 +1,47 @@
+//
+//  NavigationStackApp.swift
+//  FitnessAgent
+//
+//  Created by Vincent Joy on 24/05/25.
+//
+
+import SwiftUI
+
+struct NavigationStackApp: View {
+    
+    @State private var navigationPath = NavigationPath()
+    @State private var users = [
+        User(name: "John Doe", age: 34),
+        User(name: "Cobb Joe", age: 45),
+        User(name: "Lena Queen", age: 26)
+    ]
+    
+    var body: some View {
+        NavigationStack(path: $navigationPath) {
+            VStack {
+                Text("Navigation Stack")
+                    .font(.title)
+                    .padding()
+                
+                List(users) { user in
+                    NavigationLink(value: user) {
+                        Text(user.name)
+                            .font(.headline)
+                    }
+                    .padding()
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(8)
+                }
+            }
+            .navigationTitle("Users")
+            
+            .navigationDestination(for: User.self) { user in
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStackApp()
+}
