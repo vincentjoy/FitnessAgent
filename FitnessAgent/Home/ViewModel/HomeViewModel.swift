273aabc02bb17cc6d7903d4a82921f8fbe0473fe
Vincent Joy <vincentjoyhere@gmail.com>
2025-05-28 20:16:13 +0530

    did set up the health manager

--- FitnessAgent/Home/ViewModel/HomeViewModel.swift
+++ FitnessAgent/Home/ViewModel/HomeViewModel.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 @Observable class HomeViewModel {
+    
+    let healthManager = HealthManager.shared
+    
     var calories: Int = 100
     var active: Int = 52
     var stand: Int = 8
@@ -25,4 +28,33 @@ import Foundation
         Workout(id: 2, title: "Walking", image: "figure.walk", tintColor: .purple, duration: "51 Mins", date: "Aug 5", calories: "319 kcal "),
         Workout(id: 3, title: "Running", image: "figure.run", tintColor: .cyan, duration: "51 Mins", date: "Sep 7", calories: "512 kcal ")
     ]
+    
+    init() {
+        
+        Task {
+            do {
+                try await healthManager.requestHealthKitAccess()
+                
+                healthManager.fetchTodayCaloriesBurned { result in
+                    switch result {
+                    case .success(let success):
+                        print(success)
+                    case .failure(let failure):
+                        print(failure.localizedDescription)
+                    }
+                }
+                
+                healthManager.fetchTodayStandHours { result in
+                    switch result {
+                    case .success(let success):
+                        print(success)
+                    case .failure(let failure):
+                        print(failure.localizedDescription)
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
 }
---
+++ FitnessAgent/Managers/HealthManager.swift
@@ -0,0 +1,78 @@
+//
+//  HealthManager.swift
+//  FitnessAgent
+//
+//  Created by Vincent Joy on 28/05/25.
+//
+
+import Foundation
+import HealthKit
+
+class HealthManager {
+    static let shared = HealthManager()
+    
+    let healthStore = HKHealthStore()
+    
+    private init() {}
+    
+    func requestHealthKitAccess() async throws {
+        let calories = HKQuantityType(.activeEnergyBurned)
+        let exercise = HKQuantityType(.appleExerciseTime)
+        let stand = HKCategoryType(.appleStandHour)
+        
+        let healthType: Set<HKObjectType> = [calories, exercise, stand]
+        try await healthStore.requestAuthorization(toShare: [], read: healthType)
+    }
+    
+    func fetchTodayCaloriesBurned(completion: @escaping (Result<Double, Error>) -> Void) {
+        let calories = HKQuantityType(.activeEnergyBurned)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: calories, quantitySamplePredicate: predicate) { _, results, error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let caloriesCount = quantity.doubleValue(for: .kilocalorie())
+            completion(.success(caloriesCount))
+        }
+        healthStore.execute(query)
+    }
+    
+    func fetchTodayExerciseTime(completion: @escaping (Result<Double, Error>) -> Void) {
+        let exercise = HKQuantityType(.appleExerciseTime)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: exercise, quantitySamplePredicate: predicate) { _, results, error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let exerciseTime = quantity.doubleValue(for: .minute())
+            completion(.success(exerciseTime))
+        }
+        healthStore.execute(query)
+    }
+    
+    func fetchTodayStandHours(completion: @escaping (Result<Int, Error>) -> Void) {
+        let stand = HKCategoryType(.appleStandHour)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKSampleQuery(sampleType: stand, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { _, results, error in
+            guard let samples = results as? [HKCategorySample], error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let standCount = samples.filter { $0.value == 0 }.count
+            completion(.success(standCount))
+        }
+        healthStore.execute(query)
+    }
+}
+
+extension Date {
+    static var startOfDay: Date {
+        let calendar = Calendar.current
+        return calendar.startOfDay(for: Date())
+    }
+}
